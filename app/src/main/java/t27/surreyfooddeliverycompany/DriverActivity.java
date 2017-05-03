79734b07eb5e7e3c2a135e225b9c6ec0e0c6b6e9
Terence Leung <Terence Leung>
2017-05-03 00:26:35 -0700

    basic connection between activities

--- app/src/main/java/t27/surreyfooddeliverycompany/DriverActivity.java
+++ app/src/main/java/t27/surreyfooddeliverycompany/DriverActivity.java
@@ -1,7 +1,9 @@
 package t27.surreyfooddeliverycompany;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DriverActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class DriverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver);
+    }
+
+    protected void profile(View view) {
+        Intent intent = new Intent(this, ProfileActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/t27/surreyfooddeliverycompany/ProfileActivity.java
+++ app/src/main/java/t27/surreyfooddeliverycompany/ProfileActivity.java
@@ -1,7 +1,9 @@
 package t27.surreyfooddeliverycompany;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ProfileActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class ProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+
+    protected void signOut (View view) {
+        //sign out
+        finish();
     }
 }
