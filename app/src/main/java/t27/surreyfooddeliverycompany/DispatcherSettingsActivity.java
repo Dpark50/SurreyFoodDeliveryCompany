5f6414ebe17901bdd4b812adb7f611664a353338
Daniel <daniel51_1994@hotmail.com>
2017-05-03 11:42:32 -0700

    connect settings activity

--- app/src/main/java/t27/surreyfooddeliverycompany/DispatcherSettingsActivity.java
+++ app/src/main/java/t27/surreyfooddeliverycompany/DispatcherSettingsActivity.java
@@ -1,13 +1,34 @@
 package t27.surreyfooddeliverycompany;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class DispatcherSettingsActivity extends AppCompatActivity {
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispatcher_settings);
+    }
+
+    public void Orders(View view) {
+        intent = new Intent(this, DispatcherActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    public void DriversList(View view) {
+        intent = new Intent(this, DriversListActivity.class);
+        finish();
+        startActivity(intent);
+    }
+
+    public void Profile(View view) {
+        intent = new Intent(this, ProfileActivity.class);
+        finish();
+        startActivity(intent);
     }
 }
