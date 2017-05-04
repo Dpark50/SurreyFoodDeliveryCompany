ea7cb68fd0ba342bb502a6a75452e44f6b44233d
Daniel <daniel51_1994@hotmail.com>
2017-05-04 11:51:59 -0700

    change selected tab colour

--- app/src/main/java/t27/surreyfooddeliverycompany/DispatcherNewOrdersActivity.java
+++ app/src/main/java/t27/surreyfooddeliverycompany/DispatcherNewOrdersActivity.java
@@ -1,6 +1,7 @@
 package t27.surreyfooddeliverycompany;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -9,11 +10,10 @@ import android.widget.TabHost;
 
 public class DispatcherNewOrdersActivity extends AppCompatActivity {
     private Intent intent;
+    private TabHost tabHost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        TabHost tabHost;
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dispatcher_new_orders);
         tabHost = (TabHost)findViewById(R.id.tabhost);
@@ -38,6 +38,27 @@ public class DispatcherNewOrdersActivity extends AppCompatActivity {
         tabHost.addTab(tab2);
         tabHost.addTab(tab3);
         tabHost.addTab(tab4);
+
+        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+            @Override
+            public void onTabChanged(String arg0) {
+                setTabColor(tabHost);
+            }
+        });
+
+        setTabColor(tabHost);
+    }
+
+    public void setTabColor(TabHost tabhost) {
+        int i = 0;
+
+        for(i = 0; i<tabhost.getTabWidget().getChildCount(); i++) {
+            tabhost.getTabWidget().getChildAt(i).setBackgroundColor(Color.TRANSPARENT);
+        }
+
+        // Change colour of the selected tab
+        tabhost.getTabWidget().getChildAt(tabhost.getCurrentTab())
+                .setBackgroundColor(Color.parseColor("#DBE4FB"));
     }
 
     public void SignOut(View view) {
