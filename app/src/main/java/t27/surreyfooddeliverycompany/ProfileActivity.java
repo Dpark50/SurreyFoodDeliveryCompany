167fafe3f0496a555d32edc48fa984525fc1f813
Daniel <daniel51_1994@hotmail.com>
2017-05-10 16:37:26 -0700

    signout firebase

--- app/src/main/java/t27/surreyfooddeliverycompany/ProfileActivity.java
+++ app/src/main/java/t27/surreyfooddeliverycompany/ProfileActivity.java
@@ -7,6 +7,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class ProfileActivity extends AppCompatActivity {
     private Intent intent;
 
@@ -17,9 +19,12 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     public void signOut (View view) {
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
         SharedPreferences preferences = getSharedPreferences(getString(
                 R.string.user_preference), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
+
+        mAuth.signOut();
         editor.clear();
         editor.apply();
 
