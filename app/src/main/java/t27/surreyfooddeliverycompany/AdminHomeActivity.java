bf2bffd63cc375f7c63438459c32f54758b45d6a
Daniel <daniel51_1994@hotmail.com>
2017-05-18 10:03:21 -0700

    delete accounts in admin

--- app/src/main/java/t27/surreyfooddeliverycompany/AdminHomeActivity.java
+++ app/src/main/java/t27/surreyfooddeliverycompany/AdminHomeActivity.java
@@ -5,7 +5,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,6 +25,8 @@ public class AdminHomeActivity extends AppCompatActivity {
     private static final String TAG = "AdminHomeActivity";
     private Intent intent;
     private DatabaseReference mDatabaseRef;
+    private DatabaseReference removeItemReference;
+    private FirebaseListAdapter<Account> adapter;
 
     private ListView listview;
     private static final int NUM_ITEMS = 10;
@@ -32,11 +38,74 @@ public class AdminHomeActivity extends AppCompatActivity {
         mDatabaseRef = FirebaseDatabase.getInstance().getReference();
         listview = (ListView) findViewById(R.id.addedAccount_listview);
 
-        //query the recent added accounts
-        Query recentAccounts_query = mDatabaseRef.child("users").orderByChild("timestampCreated").limitToLast(NUM_ITEMS);
+        changeView(findViewById(R.id.activity_admin_home));
 
-        //list adapter for added accounts
-        FirebaseListAdapter<Account> adapter =new FirebaseListAdapter<Account>(this,Account.class,R.layout.admin_account_item_layout,recentAccounts_query) {
+        registerForContextMenu(listview);
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View view, ContextMenu.ContextMenuInfo info) {
+        super.onCreateContextMenu(menu, view, info);
+        // groupID, itemId, order, title
+        menu.add(0, view.getId(), 0, "Delete account");
+        menu.add(0, view.getId(), 0, "Cancel");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item){
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        int listPosition = info.position;
+        removeItemReference = adapter.getRef(listPosition);
+
+        if (item.getTitle().equals("Delete account")){
+            removeItemReference.removeValue();
+        } else if (item.getTitle().equals("Cancel")){
+            return false;
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+    public void changeView(View view) {
+        Button tab = (Button) findViewById(R.id.tab_button);
+        String buttonName = tab.getText().toString();
+        Query query;
+
+        if (buttonName.equals("Staff")) {
+            tab.setText("Customers");
+            query = mDatabaseRef.child("driver").orderByChild("name");
+        } else {
+            tab.setText("Staff");
+            query = mDatabaseRef.child("users").orderByChild("timestampCreated")
+                    .limitToLast(NUM_ITEMS);
+
+            //query the recent added accounts
+            //Query recentAccounts_query = mDatabaseRef.child("users").orderByChild("timestampCreated")
+               //     .limitToLast(NUM_ITEMS);
+
+            //list adapter for added accounts
+            /*adapter = new FirebaseListAdapter<Account>(this,
+                    Account.class,R.layout.admin_account_item_layout,recentAccounts_query) {
+                @Override
+                protected void populateView(View view, Account account, int i) {
+                    TextView text1=(TextView)view.findViewById(R.id.email);
+                    TextView text2=(TextView)view.findViewById(R.id.name);
+                    TextView text3=(TextView)view.findViewById(R.id.type);
+                    TextView text4=(TextView)view.findViewById(R.id.phone);
+                    String email_text = "Email: " + account.getEmail();
+                    String name_text = "Name: " + account.getName();
+                    String accountType_text = "Type: " + account.getAccountType();
+                    String phone_text = "Phone: " + account.getNumber();
+                    text1.setText(email_text);
+                    text2.setText(name_text);
+                    text3.setText(accountType_text);
+                    text4.setText(phone_text);
+                }
+            };*/
+        }
+        adapter = new FirebaseListAdapter<Account>(this,
+                Account.class,R.layout.admin_account_item_layout, query) {
             @Override
             protected void populateView(View view, Account account, int i) {
                 TextView text1=(TextView)view.findViewById(R.id.email);
